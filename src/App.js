41e7b3d81c1c91ad070edb97f610fc5f1a11b553
milone1 <erickmilan1002@gmail.com>
2022-03-21 14:50:40 -0500

    Primera App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/testimonio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='contenedor-principal'>
+        <h1>Testimonios: </h1>
+        <Testimonio
+          nombre='Juan'
+          pais='LIMA-PERU'
+          imagen='juan'
+          cargo='Ingeniero de Software'
+          empresa='IBM'
+          testimonio= 'El mundo esta lleno de oportunidades solo tenemos que aprovecharlas.'/>
+        <Testimonio
+          nombre='Juanita'
+          pais='MOSCU-RUSIA'
+          imagen='juanita'
+          cargo='Ingeniera de Software'
+          empresa='ChatDesk'
+          testimonio='Nunca dejes que tus miedos sean mas grandes que tus sueños.'/>
+        <Testimonio 
+          nombre='Pedro'
+          pais='Santiago-Chile'
+          imagen='pedro'
+          cargo='Web Developer'
+          empresa='Spotify'
+          testimonio='El 50% del exito es hacerse presente.'/>
+      </div>
     </div>
   );
 }
---
+++ src/componentes/testimonio.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../style-sheets/testimonio.css';
+
+function Testimonio(props) {
+  return (
+    <div className='contenedor-testimonio'>
+      <img 
+        className='imagen-testimonio'
+        src={require(`../img/testimonio-${props.imagen}.jpeg`)}
+        alt='Foto de Emma'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className='cargo-testimonio'>
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
